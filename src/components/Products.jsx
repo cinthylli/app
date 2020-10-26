45065e16e3c5158c7cd7dba3540411d5f78ce54c
Cinthya Cabanzo <cinthya.cabanzo@gmail.com>
2020-10-25 23:05:59 -0500

    paginacion

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,13 +4,11 @@ import Card from '../components/Card'
 import useFetchProducts from '../hooks/useFetchProducts';
 
 
-function Products({ category, points,setPoints }) {
+function Products({ category, points, setPoints }) {
 
     const { data: products, loading } = useFetchProducts("");
 
     const [categoryProducts, setcategoryProducts] = useState([])
-
-
 
     useEffect(() => {
         if (category === "" || category === undefined) {
@@ -18,15 +16,62 @@ function Products({ category, points,setPoints }) {
         } else {
             setcategoryProducts(products.filter(product => product.category === category))
         }
-    }, [category,points])
+    }, [category])
+
+    const itemsPerPage = 16;
+    const [currentPage, setcurrentPage] = useState(1);
+    const [indexOfLastItem, setIndexOfLastItem] = useState(itemsPerPage);
+    const [indexOfFirstItem, setIndexOfFirstItem] = useState(0);
+    const [currentItems, setCurrentItems] = useState(products);
+
+    useEffect(() => {
+        async function fetchData() {
+            await setIndexOfFirstItem(itemsPerPage *  (currentPage-1));
+            await setIndexOfLastItem(itemsPerPage *  (currentPage));
+            await setCurrentItems(categoryProducts.slice({indexOfFirstItem, indexOfLastItem}));
+            
+        }
+        fetchData();
+    }, [currentPage,itemsPerPage])
+
+    const handleClick = (e) => {
+        setcurrentPage(e.target.id)
+        console.log(currentPage);
+    }
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(categoryProducts.length / itemsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+        return (
+          <li
+            key={number}
+            id={number}
+            onClick={handleClick}
+          >
+            {number}
+          </li>
+        );
+      });
 
     return (
         <>
             <h1>{category || "Products"}</h1>
+
+            {console.log("category > ", category)}
+            {console.log("currentItems > ", currentItems)}
+            {console.log("currentPage > ", currentPage)}
+            {console.log("indexOfFirstItem > ", indexOfFirstItem)}
+            {console.log("indexOfLastItem > ", indexOfLastItem)}
+            
+            {console.log("categoryProducts slide >>>>>>>> ",categoryProducts.slice(indexOfFirstItem,indexOfLastItem))}
+            {console.log("categoryProducts slide > ",categoryProducts.slice(16,32))}
             {loading && "cargando....."}
             <section>
                 {
-                    categoryProducts.map((product) => {
+                    categoryProducts.slice(indexOfFirstItem,indexOfLastItem).map((product) => {
                         return (
                             <Card
                                 category={product.category}
@@ -42,6 +87,9 @@ function Products({ category, points,setPoints }) {
                     })
                 }
             </section>
+            <ul id="page-numbers">
+              {renderPageNumbers}
+            </ul>
         </>
     )
 }
